eaffa6f2cd99165f5a2a5da6967f024a2ed8d1f7
hayoung <ha02e@naver.com>
2024-03-26 19:16:49 +0900

    라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Products from "./page/Products";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
+
+//1. 전체 상품 페이지, 로그인 페이지, 상품 상세 페이지
+//2. 전체 상품 페이지 - 전체 상품 보기
+//3. 로그인 페이지 - 로그인 버튼을 누르면 로그인 페이지로 이동, 상품 디테일 눌렀을 때 로그인 안됐을 경우 로그인 페이지로 이동
+//4. 로그인이 되어있으면 상품 상세 페이지로 이동
+//5. 로그아웃 버튼을 누르면 로그아웃이 된다.
+//6. 로그아웃이 되면 상품 상세 페이지를 볼 수 없다.(다시 로그인 페이지로 이동)
+//7. 로그인 상태면 로그아웃 버튼이 보이고, 로그인이 안되어있으면 로그인 버튼이 보인다.
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
