d8bb2544304c0581308b61c485d3a715565713af
hayoung <ha02e@naver.com>
2024-03-27 16:27:42 +0900

    choice를 bestseller로 수정

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -3,10 +3,10 @@ import React from "react";
 const ProductCard = ({ item }) => {
   return (
     <div className="product-card">
+      {item?.choice === true && <div className="product-best">BestSeller</div>}
       <div className="product-image">
-        <img src={item?.img} />
+        <img src={item?.img} alt="product img" />
       </div>
-      <div>{item?.choice === true ? "Conscious choice" : ""}</div>
       <div className="product-name">{item?.title}</div>
       <div className="product-price">
         &#8361;&nbsp;{(item?.price).toLocaleString("ko-KR")}
